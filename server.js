8407ac4725e09784bbf4c626db4697c91d26ba21
Kshitij <kshitijnath@gmail.com>
2021-05-18 11:06:37 +0530

    Connected to the database

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express();
+require('dotenv').config()
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('API running'))
+
+// Connecting to the Database
+const url = `mongodb+srv://kshitij:${process.env.DB_PASSWORD}@cluster0.ihxoq.mongodb.net/DEVDB?retryWrites=true&w=majority`
+mongoose.connect(url, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("Database Connected")).catch(err => console.log("Could not connect to DB"))
 
 app.listen(PORT, () => console.log("Listening on port " + PORT))
