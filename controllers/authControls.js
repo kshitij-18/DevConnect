e3c0db800b83e18b763dd03a3e71cdd03c8edff7
Kshitij <kshitijnath@gmail.com>
2021-05-18 19:11:19 +0530

    Implemented Login

--- controllers/authControls.js
+++ controllers/authControls.js
@@ -1,7 +1,51 @@
+const User = require('../Database/userdb')
+const bcrypt = require('bcryptjs')
+const { validationResult } = require('express-validator')
+const jwt = require('jsonwebtoken')
+
 const authController = {
-    login: (req, res) => {
-        res.send("Here comes your login")
+    showUser: async (req, res) => {
+        try {
+            let user = await User.findById(req.user.id).select('-password')
+            res.status(200).json(user)
+        } catch (error) {
+            console.log(error.message)
+            res.status(500).send("Server Error")
+        }
+    },
+    Login: async (req, res) => {
+        // console.log(req.body)
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
+        // Destructuring what is coming from the Request (the form basically)
+        const { email, password } = req.body
+
+        // See if user exists
+        let user = await User.findOne({ email })
+        if (!user) {
+            return res.status(401).json({ errors: [{ msg: "User does not exist" }] })
+        }
+
+        const verified = await bcrypt.compare(password, user.password)
+        if (!verified) {
+            return res.status(401).json({ errors: [{ msg: "Please enter a valid password" }] })
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+        jwt.sign(payload,
+            process.env.JWT_SECRET,
+            { expiresIn: 3600000 },
+            (err, token) => {
+                if (err) throw err
+                res.json({ token: token })
+            })
     }
 }
-
 module.exports = authController
--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,22 @@ const express = require('express')
 const router = express.Router();
 const authController = require('../controllers/authControls')
 const verifyToken = require('../middleware/auth')
+const { body } = require('express-validator')
 
 // @route GET /api/users
 // @desc Gets all the users in the database
 // @access PUBLIC
-router.get('/', verifyToken, authController.login)
+router.get('/', verifyToken, authController.showUser)
+
+// @route POST /api/auth
+// @desc Signs and gives a token back for the user (Sign in)
+// @access PUBLIC
+router.post('/',
+    [
+        body('email', 'Please enter a valid email').isEmail(),
+        body('password', 'Password is required').exists()
+    ],
+    authController.Login
+)
 
 module.exports = router
