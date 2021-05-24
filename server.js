073f5d1e6fa908f4c7b9a47b1efd4ed4eff06d80
Kshitij <kshitijnath@gmail.com>
2021-05-24 22:57:10 +0530

    Started to work on the frontend and performed cleanup

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 require('dotenv').config()
 const mongoose = require('mongoose')
+const cors = require('cors')
 const userRoutes = require('./routes/users')
 const authRoutes = require('./routes/auth')
 const profileRoutes = require('./routes/profile')
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json())
+app.use(cors())
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes)
 app.use('/api/profile', profileRoutes)
