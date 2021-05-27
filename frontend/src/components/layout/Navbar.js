2e114c999a377226f865f557b8ea254b06f276ac
Kshitij <kshitijnath@gmail.com>
2021-05-27 12:13:54 +0530

    Initial Try

---
+++ frontend/src/components/layout/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+
+const Navbar = () => {
+    return (
+        <div>
+            <AppBar color="inherit" position="sticky">
+                <h1>Hello</h1>
+            </AppBar>
+        </div>
+    )
+}
+
+export default Navbar
